87eeb7461b35d76a3639f1461548cdb97370cfd9
leonardo <marchesini.leonardo2002@gmail.com>
2021-08-10 11:36:32 +0200

    .

--- Resources/views/layouts/homepage/category.blade.php
+++ Resources/views/layouts/homepage/category.blade.php
@@ -1,8 +1,16 @@
+@php
+    //dddx(get_defined_vars());
+@endphp
 @foreach (XotModel('tag')::where('parent_id', 1)->get() as $row)
     <div class="w-100"
         style="background: #e6e6e6a4;margin-left: 0px;width: 314px;border-radius: 15px;margin-bottom: 20px;">
-        <a href="{{ route('container0.index', ['lang' => App::getLocale(), 'container0' => 'product', 'id' => $row->id]) }}"
-            style="text-decoration: none; color: rgb(39, 39, 39)">
+        @php
+        //<a href="{{ route('container0.index', ['lang' => App::getLocale(), 'container0' => 'product', 'id' => $row->id]) }}"
+            //dddx();
+            //$profile_panel->relatedUrl(['related_name' => 'bell_boy', 'act' => 'index_edit'])
+            ///it/tags/le-classiche/products
+        @endphp
+        <a href="{{ route('container0.index', ['lang' => App::getLocale(), 'container0' => 'product', 'id' => $row->id]) }}" style="text-decoration: none; color: rgb(39, 39, 39)">
             <div class="w-100"><img class="w-100"
                     src="{{ $row->image_src }}"
                     style="width: 280px;margin: 0px;border-radius: 15px;margin-bottom: 0;padding: 7px;">
