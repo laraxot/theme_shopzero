0b637212955465730d44d3bf03ce7e1da26cd977
Davide <davidecavallini1987@gmail.com>
2021-08-13 15:55:47 +0200

    aggiunti dati dei prodotti alle blades

--- Resources/views/products/itemCats/productItems/index.blade.php
+++ Resources/views/products/itemCats/productItems/index.blade.php
@@ -1,3 +1,18 @@
+
+@php
+
+$categoryModel=$_panel->getParent()->getParent()->getParent()->row;
+
+$categoryGuid=$categoryModel->guid;
+$categoryTitle=$categoryModel->title;
+
+$productModel=$_panel->getParent()->getParent()->row;
+
+$product=['title'=>$productModel->title,'firstPrice'=>$productModel->prices()->first()->price];
+
+
+@endphp
+
 @include('pub_theme::layouts.global.head')
 
 {{-- dddx(get_defined_vars()) --}}
@@ -5,14 +20,14 @@
 <body style="background-color: #f8f9fa;">
     <div class="w-100" style="background-color: #f8f9fa;padding: 11px;">
         @include('pub_theme::layouts.global.topbar')
-        @include('pub_theme::layouts.product_page.product_page_title')
+        @include('pub_theme::layouts.product_page.product_page_title',['categoryGuid'=>$categoryGuid])
         <div style="padding-bottom: 10px;">
             {{-- @include('pub_theme::layouts.product_page.expand') --}}
             {{-- @livewire('fastorder::test')--}}
             @include('pub_theme::addition.additions')
         </div>
     </div>
-    @include('pub_theme::layouts.product_page.add_cart')
+    @include('pub_theme::layouts.product_page.add_cart',['product'=>$product])
     @include('pub_theme::layouts.global.foot')
     
    
